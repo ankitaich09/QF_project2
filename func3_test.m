3fa63ab703d6fa01ffa1d2e6c701c4dcbdd52f81
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2018-10-27 20:03:04 -0400

    uses func3 functions defined in files

--- func3_test.m
+++ func3_test.m
@@ -1,18 +1,11 @@
-f3 = @(x) 100 * (x(2,:) - x(1,:).^2).^2 + (1 - x(1,:)).^2;
-
-f3dx1 = @(x) 2 * x(1,:) - 400 * x(1,:) * (x(2,:) - x(1,:).^2) - 2;
-f3dx2 = @(x) 200 * x(2,:) - 200 * x(1,:).^2;
-
-gf3 = @(x) [f3dx1(x); f3dx2(x)];
-
 [x, y] = meshgrid(-2:0.1:2);
 % all points arranged as X = [x1, ..., xn] from the meshgrid
 X = [x(:)'; y(:)'];
 fig1 = figure
 % makes z a grid of values with size of x
-z = reshape(f3(X), size(x));
+z = reshape(func3(X), size(x));
 surf(x, y, z);
-pxy = numerical_gradient_func(f3, X,1);
+pxy = numerical_gradient_func(@func3, X,1);
 figure
 % contour only near zero
 contour(x, y, z, 0:.01:2);
@@ -32,10 +25,10 @@ known_minimum = [1; 1];
 % analytical
 % [errors, points] = gradient_descent(f3, gf3, [0; 0], 10000, 0.0000000001, known_minimum)
 % numerical
-[errors, points] = gradient_descent_method(f3, gf3, [-2; -2], 10000, 0.0000000001, known_minimum)
+[errors, points] = gradient_descent_method(@func3, @func3gradient, [-2; -2], 10000, 0.0000000001, known_minimum)
 draw_trace(points);
 figure(fig1)
 hold on
-draw3_trace(points, f3);
+draw3_trace(points, @func3);
 figure
 draw_errors(errors, "log")
