27a1e6cc46a7676798b10a3f44026cf82d2893ce
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2018-10-28 00:25:35 -0400

    changes description of function

--- gradient_descent_method.m
+++ gradient_descent_method.m
@@ -1,10 +1,11 @@
 function [errors, points] = gradient_descent_method(f, g, x0, maxiter, E, varargin)
-    % f is a function such that f(x) : Rnx1 -> R
-    % g is the gradient of f, g(x) : Rnx1 -> Rnx1
-    % x0 is the initial point, belongs to Rnx1
-    % maxiter is the maximum number of iterations
-    % E is acceptable error between iteration
-    % vargarin can include a point x* (xstar) for a known minimum
+    % f: is a function such that f(x) : Rnx1 -> R
+    % g: is the gradient of f, g(x) : Rnx1 -> Rnx1
+    % x0: is the initial point, belongs to Rnx1
+    % maxiter: is the maximum number of iterations
+    % E: is the acceptable error between iterations
+    % vargarin: can include a point x* (xstar) for a known minimum so the
+    % error is computed with that minimum
     % this function returns the error in each iteration and the points found
 
     if(nargin>5)
