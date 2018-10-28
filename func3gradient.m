c1e0aac88e4c9787cdb5e92574fd2627354d405e
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2018-10-28 13:42:17 -0400

    fixes typos

--- func3gradient.m
+++ func3gradient.m
@@ -1,6 +1,6 @@
 function gf3 = func3gradient(x)
 	% x belongs to R2xm where each column is a vector 2x1
-	% returns an array of vector with the gradien for each point xi
+	% returns an array of vectors with the gradient for each point xi
 	f3dx1 = @(x) 2 .* x(1,:) - 400 .* x(1,:) .* (x(2,:) - x(1,:).^2) - 2;
 	f3dx2 = @(x) 200 .* x(2,:) - 200 .* x(1,:).^2;
 
